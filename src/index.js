98869551a3ae449adcda333dadaca1499c0dbc45
starmastar1126 <starmastar1126@gmail.com>
2026-02-23 06:45:58 -0800

    updated deploy.yml

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './sass/index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/company">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
